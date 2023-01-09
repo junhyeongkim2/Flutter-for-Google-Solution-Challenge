5261fcdf32568e05458c5d691074eae44aab5c2a
jun <ggprgrkjh@naver.com>
2023-01-10 01:44:48 +0900

    feat: separate feed

--- instagram/lib/home_page.dart
+++ instagram/lib/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:instagram/feed.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -25,64 +26,6 @@ class HomePage extends StatelessWidget {
           centerTitle: true,
           backgroundColor: Colors.white,
         ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // 이미지
-            Image.network(
-              "https://cdn2.thecatapi.com/images/kat_7kqBi.png",
-              height: 400,
-              width: double.infinity,
-              fit: BoxFit.cover,
-            ),
-            Row(
-              children: [
-                IconButton(
-                  icon: Icon(CupertinoIcons.heart, color: Colors.black),
-                  onPressed: () {},
-                ),
-                IconButton(
-                  icon: Icon(CupertinoIcons.chat_bubble, color: Colors.black),
-                  onPressed: () {},
-                ),
-                Spacer(),
-                IconButton(
-                  icon: Icon(CupertinoIcons.bookmark, color: Colors.black),
-                  onPressed: () {},
-                ),
-              ],
-            ),
-
-            // 좋아요
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                "2 likes",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-
-            // 설명
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                "My cat is docile even when bathed. I put a duck on his head in the wick and he's staring at me. Isn't it so cute??",
-              ),
-            ),
-
-            // 날짜
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                "FEBURARY 6",
-                style: TextStyle(
-                  color: Colors.grey,
-                ),
-              ),
-            ),
-          ],
-        ));
+        body: Feed());
   }
 }
