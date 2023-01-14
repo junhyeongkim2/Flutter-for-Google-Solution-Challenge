992ba9a8ca0f269360dc38321cdfd66793cffede
jun <ggprgrkjh@naver.com>
2023-01-15 00:50:09 +0900

    feat: shared preferences setting

--- onboarding/lib/main.dart
+++ onboarding/lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:introduction_screen/introduction_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
+late SharedPreferences prefs;
+void main() async {
+  // main() 함수에서 async를 쓰려면 필요
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // shared_preferences 인스턴스 생성
+  prefs = await SharedPreferences.getInstance();
+
   runApp(MyApp());
 }
 
