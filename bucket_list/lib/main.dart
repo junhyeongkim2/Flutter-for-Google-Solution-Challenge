cfeb9e753f613421aebaef9be50a3618de5d5f4f
jun <ggprgrkjh@naver.com>
2023-02-09 02:17:30 +0900

    feat : bucketlis update

--- bucket_list/lib/main.dart
+++ bucket_list/lib/main.dart
@@ -17,6 +17,14 @@ class MyApp extends StatelessWidget {
   }
 }
 
+/// 버킷 클래스
+class Bucket {
+  String job; // 할 일
+  bool isDone; // 완료 여부
+
+  Bucket(this.job, this.isDone); // 생성자
+}
+
 /// 홈 페이지
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -26,7 +34,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List<String> bucketList = ['여행가기']; // 전체 버킷리스트 목록
+  List<Bucket> bucketList = []; // 전체 버킷리스트 목록
 
   @override
   Widget build(BuildContext context) {
@@ -39,13 +47,17 @@ class _HomePageState extends State<HomePage> {
           : ListView.builder(
               itemCount: bucketList.length, // bucketList 개수 만큼 보여주기
               itemBuilder: (context, index) {
-                String bucket = bucketList[index]; // index에 해당하는 bucket 가져오기
+                Bucket bucket = bucketList[index]; // index에 해당하는 bucket 가져오기
                 return ListTile(
                   // 버킷 리스트 할 일
                   title: Text(
-                    bucket,
+                    bucket.job,
                     style: TextStyle(
                       fontSize: 24,
+                      color: bucket.isDone ? Colors.grey : Colors.black,
+                      decoration: bucket.isDone
+                          ? TextDecoration.lineThrough
+                          : TextDecoration.none,
                     ),
                   ),
                   // 삭제 아이콘 버튼
@@ -58,7 +70,9 @@ class _HomePageState extends State<HomePage> {
                   ),
                   onTap: () {
                     // 아이템 클릭시
-                    print('$bucket : 클릭 됨');
+                    setState(() {
+                      bucket.isDone = !bucket.isDone;
+                    });
                   },
                 );
               },
@@ -73,7 +87,8 @@ class _HomePageState extends State<HomePage> {
           );
           if (job != null) {
             setState(() {
-              bucketList.add(job);
+              Bucket newBucket = Bucket(job, false);
+              bucketList.add(newBucket);
             });
           }
         },
