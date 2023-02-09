5668251ed73d4da14a56b4078fc695a4ce275e73
jun <ggprgrkjh@naver.com>
2023-02-10 04:06:34 +0900

    feat : provider create

--- bucket_list_with_provider/lib/bucket_service.dart
+++ bucket_list_with_provider/lib/bucket_service.dart
@@ -7,4 +7,10 @@ class BucketService extends ChangeNotifier {
   List<Bucket> bucketList = [
     Bucket('잠자기', false), // 더미(dummy) 데이터
   ];
+  void createBucket(String job) {
+    bucketList.add(Bucket(job, false));
+    notifyListeners();
+  }
 }
+
+/// bucket 추가
